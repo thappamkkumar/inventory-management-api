2b5571b1435c02e82e2bfa56b503afb2b8e454ea
MUKESH KUMAR <thappamkkumar@gmail.com>
2026-07-18 15:12:09 +0530

    chore: initialize Laravel inventory management API

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::get('/health', function () {
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    });
+});
