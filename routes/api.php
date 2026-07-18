76bc25c885e1b89330816f44c120ca88bc0c875e
MUKESH KUMAR <thappamkkumar@gmail.com>
2026-07-18 15:15:52 +0530

    refactor: organize API routes under v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+*/
+
 
 Route::prefix('v1')->group(function () {
     Route::get('/health', function () {
         return response()->json([
             'status' => 'ok'
         ]);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
     });
 });
