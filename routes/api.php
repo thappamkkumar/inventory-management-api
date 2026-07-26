38b28e9da6bc3fd27c811234eb1aaded3e25cee2
MUKESH KUMAR <thappamkkumar@gmail.com>
2026-07-26 19:01:38 +0530

    feat: scaffold category module

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
-
+use App\Http\Controllers\Api\CategoryController;
 
 /*
 Route::get('/user', function (Request $request) {
@@ -30,6 +30,13 @@ Route::prefix('v1')->group(function () {
         Route::apiResource('products', ProductController::class)->names('products')
             ->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::patch('/products/{id}/restore', [ProductController::class, 'restore'])->name('products.restore');
+
+        Route::apiResource('categories', CategoryController::class)->names('categories')
+            ->only(['index', 'store', 'show', 'update', 'destroy']);
+        Route::patch(
+            '/categories/{id}/restore',
+            [CategoryController::class, 'restore']
+        )->name('categories.restore');
 
     });
 
