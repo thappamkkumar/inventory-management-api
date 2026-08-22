b49abf6453206de9383ff8c40473f2e3653f3f96
Mukesh Kumar <thappamkkumar@gmail.com>
2026-08-22 12:37:45 +0530

    change request validation response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,5 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function ($request, $input) {
+            return $request->is('api/*') || $request->expectsJson();
+        });
     })->create();
